9858d41a00b8f0e54f12b90abad1ea982c0ee464
mickeyminion14 <sarthakagrawal6@gmail.com>
2020-04-25 16:23:36 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+  ],
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,102 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
+  @ViewChild("barChart") barChart;
+  @ViewChild("barChart1") barChart1;
+  @ViewChild("barChart2") barChart2;
 
-  constructor(public navCtrl: NavController) {
+  bars: any;
+  colorArray: any;
 
+  ionViewDidEnter() {
+    this.createBarChart();
   }
 
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"],
+        datasets: [
+          {
+            label: "Viewers in millions",
+            data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+            backgroundColor: "rgb(0,0,0,0)", // array should have same number of elements as number of dataset
+            borderColor: "rgb(255, 204, 255)", // array should have same number of elements as number of dataset
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+    this.bars = new Chart(this.barChart1.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"],
+        datasets: [
+          {
+            label: "Viewers in millions",
+            data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+            backgroundColor: "rgb(0,0,0,0)", // array should have same number of elements as number of dataset
+            borderColor: "rgb(255, 128, 0)", // array should have same number of elements as number of dataset
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+    this.bars = new Chart(this.barChart2.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"],
+        datasets: [
+          {
+            label: "Viewers in millions",
+            data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+            backgroundColor: "rgb(0,0,0,0)", // array should have same number of elements as number of dataset
+            borderColor: "rgb(102, 204, 0)", // array should have same number of elements as number of dataset
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
 }
